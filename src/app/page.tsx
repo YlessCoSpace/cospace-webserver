bb77128d736bc0df60d6b5f32082c82836b87317
saimkrua <Sataporn.k@houseofdev.tech>
2024-10-05 21:51:38 +0700

    [EDIT] add scale

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
   return (
     <div className="w-full px-32 py-16 flex flex-col gap-8">
       <p className="text-slate-900 font-bold text-[40px]">Room 502</p>
-      <div className="px-24 flex flex-row justify-between">
-        <SeatMapStage tables={tables?.tables || []} />
+      <div className="flex flex-row justify-center">
+        <SeatMapStage tables={tables?.tables || []} max_x={tables?.max_x || 13.5} max_y={tables?.max_y || 11.5} />
       </div>
     </div>
   );
--- src/components/SeatMapStage.tsx
+++ src/components/SeatMapStage.tsx
@@ -1,31 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Stage, Layer, Circle, Text, Rect } from "react-konva";
 
 // Helper function to generate mini circles for people
-const getPeopleCircles = (t: TableNode) => {
+const getPeopleCircles = (t: TableNode, scale: number) => {
   const peopleCount = Math.min(t.people, 8); // Show maximum of 8 people
   const angleStep = (2 * Math.PI) / peopleCount; // Divide circle into equal angles
   const radius = 70; // Distance from the table to the mini circles
 
   return [...Array(peopleCount)].map((_, index) => {
     const angle = index * angleStep;
-    const x = t.x + radius * Math.cos(angle);
-    const y = t.y + radius * Math.sin(angle);
+    const x = t.x * scale + radius * Math.cos(angle);
+    const y = t.y * scale + radius * Math.sin(angle);
 
     return <Circle key={index} x={x} y={y} radius={10} fill="blue" />;
   });
 };
 
-export default function SeatMapStage({ tables }: { tables: TableNode[] }) {
+export default function SeatMapStage({ tables, max_x, max_y }: { tables: TableNode[], max_x: number, max_y: number }) {
+  const scale = 1000 / max_x
+  
   return (
-    <Stage width={1000} height={500}>
+    <Stage width={max_x * scale} height={max_y * scale}>
       <Layer>
         {/* Border rectangle */}
         <Rect
           x={0}
           y={0}
-          width={1000}
-          height={500}
+          width={max_x * scale}
+          height={max_y * scale}
           stroke="black"
           strokeWidth={4}
           fill="transparent"
@@ -35,8 +37,8 @@ export default function SeatMapStage({ tables }: { tables: TableNode[] }) {
           <React.Fragment key={t.id}>
             {/* Main table circle */}
             <Circle
-              x={t.x}
-              y={t.y}
+              x={t.x * scale}
+              y={t.y * scale}
               radius={50}
               fill={t.people > 0 ? "red" : t.item ? "yellow" : "green"}
               stroke="black"
@@ -44,11 +46,11 @@ export default function SeatMapStage({ tables }: { tables: TableNode[] }) {
             />
 
             {/* Mini circles for people */}
-            {t.people > 0 && getPeopleCircles(t)}
+            {t.people > 0 && getPeopleCircles(t, scale)}
 
             {/* Show bag icon if people <= 0 and item is true */}
             {t.people <= 0 && t.item && (
-              <Text x={t.x - 25} y={t.y - 15} text="👜" fontSize={40} />
+              <Text x={t.x * scale - 25} y={t.y * scale - 15} text="👜" fontSize={40} />
             )}
           </React.Fragment>
         ))}
